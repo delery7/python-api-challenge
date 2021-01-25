c2c29b9a06f5c8ea5e0cf2fbd2c8d581b2f29d98
Dustin Elery <delery7@yahoo.com>
2021-01-25 18:47:48 -0500

    Got the first heatmap done, stuck on filtering data frame, try diff approach

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "898edccfede735d170c3bc7e3a6f1f17"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyCvSakltfVC7mJJX9qI5D-2h7gNQwKWeI8"
